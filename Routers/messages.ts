216853dc6e5ebc9c3a68cbec9ef83696e82232c5
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-08-06 05:30:55 +0600

    get запрос работает

--- Routers/messages.ts
+++ Routers/messages.ts
@@ -19,4 +19,20 @@ routerMessage.post('/', async (req, res) => {
         res.status(500).send(e);
     }
 });
+
+routerMessage.get('/', async (req, res) => {
+    try {
+        const messages: Imessage[] = [];
+        const files = await fs.readdir(path);
+        for (const file of files) {
+            const message = await fs.readFile(`${path}/${file}`);
+            messages.push(JSON.parse(message.toString()));
+        }
+        res.send(messages.reverse().slice(0, 5));
+    } catch (e) {
+        console.error('Error reading messages');
+        res.status(500).send(e);
+    }
+});
+
 export default routerMessage;
